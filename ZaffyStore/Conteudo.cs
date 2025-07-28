8d59da7be2861976f8342c3e44c4a4bf10c3550d
Robertinhou <robertmenezesp9@gmail.com>
2025-07-28 08:36:59 -0300

    Estrutura MAIN do projeto

---
+++ ZaffyStore/Conteudo.cs
@@ -0,0 +1,15 @@
+namespace ZaffyStore
+{
+    public partial class Conteudo : Form
+    {
+        public Conteudo()
+        {
+            InitializeComponent();
+        }
+
+        private void Conteudo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
