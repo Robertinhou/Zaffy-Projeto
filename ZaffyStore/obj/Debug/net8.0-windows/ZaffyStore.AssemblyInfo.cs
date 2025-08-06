d65cda0fbe8a2303404353e3b789353edaa53492
Robertinhou <robertmenezesp9@gmail.com>
2025-08-06 12:42:33 -0300

    style: tela perfil | feat: botões de editar e salvar, API de cep

--- ZaffyStore/obj/Debug/net8.0-windows/ZaffyStore.AssemblyInfo.cs
+++ ZaffyStore/obj/Debug/net8.0-windows/ZaffyStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZaffyStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dca934e43b71b28c08ac36293a784db3a6788ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68085abb2d9886da332fa87cf87a6935a7c27d02")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZaffyStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZaffyStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
