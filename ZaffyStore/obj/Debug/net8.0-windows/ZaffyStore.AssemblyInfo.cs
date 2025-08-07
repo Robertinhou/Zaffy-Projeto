511865e74de8fbd0367f89ada6fcb635321d77b4
Robertinhou <robertmenezesp9@gmail.com>
2025-08-07 13:39:46 -0300

    Commit para commit

--- ZaffyStore/obj/Debug/net8.0-windows/ZaffyStore.AssemblyInfo.cs
+++ ZaffyStore/obj/Debug/net8.0-windows/ZaffyStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZaffyStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7367537645ed12ae95e32906cfddcd2dae4b0bd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc95ba0e151f2157c2d69748932c9acd0ad34531")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZaffyStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZaffyStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
